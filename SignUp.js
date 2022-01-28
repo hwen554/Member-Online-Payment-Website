525c991b596df8066936fe8c0024e7683044c5dc
Mr.Hong Weng <78581470+hwen554@users.noreply.github.com>
2022-01-28 17:39:18 +1300

    Add files via upload

---
+++ SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+
+export default function SignUp() {
+  return (
+    
+    <video src='/videos/video-3.mp4' autoPlay loop muted />
+    
+    
+  );
+}
