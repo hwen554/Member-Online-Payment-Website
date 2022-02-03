ecb6e4856465911e8faaccb0f66d2dbc1ff1dbf2
hwen554 <78581470+hwen554@users.noreply.github.com>
2022-02-03 17:44:11 +1300

    Update Home.js
    update

--- Home.js
+++ Home.js
@@ -3,6 +3,7 @@ import '../../App.css';
 import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
+import Member from '../Member';
 
 function Home() {
   return (
