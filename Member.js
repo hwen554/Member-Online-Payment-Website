02f96dbec7fc9ddbb8380aacb187cd9daa5ff3da
Hong Weng <78581470+hwen554@users.noreply.github.com>
2022-12-17 16:55:00 +1300

    Update Member.js

--- Member.js
+++ Member.js
@@ -5,7 +5,7 @@ import { FaFireAlt } from 'react-icons/fa';
 
 import {Button} from './Button';
 
-zxhzxhzxhzxhzhxzxhzxhzxhzxhzxhzxh
+zxhzxhzxhzxhzhxzxhzxhzxhzxhzxh
 function Member(){
     return (
         <div>
